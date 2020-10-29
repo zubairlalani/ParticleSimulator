fb1faa6146136464dfa48a5112129fd9f27bfc0a
zubairlalani <zubairlalani07@gmail.com>
2020-10-28 22:41:13 -0400

    Configure CMakeLists.txt file

---
+++ apps/simulation_main.cc
@@ -0,0 +1,11 @@
+//
+// Created by Zubair Lalani on 10/28/20.
+//
+#include <string>
+#include <iostream>
+
+using std::string;
+int main(){
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
